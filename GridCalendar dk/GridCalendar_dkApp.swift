3326ad0d47827e8b6795963819f484a58f0241ef
Ogieen <kasper610@gmail.com>
2023-01-02 19:34:22 +0100

    Initial Commit

---
+++ GridCalendar dk/GridCalendar_dkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendar_dkApp.swift
+//  GridCalendar dk
+//
+//  Created by Damian Kasperowicz on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendar_dkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
